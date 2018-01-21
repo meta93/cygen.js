5780c990dd7a684918c31893b4d4644caf306039
Dominic Damoah <Dominic.imakestuff@gmail.com>
2018-01-21 04:21:30 +0000

    Fixing an error with my authorship
    Signed-off-by: Dominic Damoah <Dominic.imakestuff@gmail.com>

--- dist/cygen.js
+++ dist/cygen.js
@@ -1,6 +1,6 @@
-/*! Cygen.js - v0.1 - 2016-04-21
-* https://github.com/Dominic-Damoah/Cygen.js
-* Copyright (c) 2016 Damoah Dominic; Licensed  */
+/*! Cygen.js - v1.1.0 - 2018-01-21
+* https://github.com/iongrahpix/Cygen.js
+* Copyright (c) 2018 Damoah Dominic; Licensed  */
 (function (root, factory) {
     'use strict';
     if (typeof define === 'function' && define.amd) {
@@ -13,11 +13,99 @@
 }(this, function () {
     'use strict';
     function Cygen() {
-        this.say_hello  = function(){
-            return "Hello";
+
+        const version = "1.1.0";
+        const INCOMING = "incoming";
+        const OUTGOING = "outgoing";
+        const NORMAL = "normal";
+        const SINGLE = "single";
+        const DEV_COLOR = "#000000";
+        const PROD_COLOR = "#68BDF6";
+        const EASTER_COLOR = "#FF756E";
+        const NODE_FILL = DEV_COLOR;
+
+        var instance = null;
+        var TGRAPH = null;
+        var cypher_text = null;
+        var fragments = [];
+        var myCodeMirror;
+        var conn_gen = true;
+        var bidirectional_gen = false;
+        var pseudo_gen = false;
+        var optimize_query = false;
+        var non_directed_mix_full = false;
+        var non_directed_mix = false;
+
+        this.show_version = function(){
+            return "Version: "+version;
         };
         
+        this.generate_query = function(input_graph){
+            if(input_graph.nodes.length == 0) {
+                console.err("No Nodes in your Query");
+                return;
+            }
+            // ** Starting Point ** //
+            TGRAPH = input_graph;
+            fragments = []; // Empty the fragments array
+            //worker_function(TGRAPH); // Recursion
+            if(fragments.length > 0){
+                var cq = combine_fragments(fragments);
+                console.log("New Query: "+cq);
+                return cq; // generated query for the Neo4j Database
+            }
+            console.err("Internal Fragments returned empty");
+            return; //
+        };
+
+        this.find_isolated_nodes  = function(tgraph){
+            
+        };
+
+        this.generate_random_query_graph = function(options){
+            
+        }
+
+        this.is_graph_flagged = function(options){
+            
+        }
+
+        this.generate_node_frags = function(){
+
+        };
+
+        this.generate_chains = function(){};
+
+        this.generate_cypher = function(){};
+
+        this.generate_fragment = function(){};
+
+        this.generate_single = function(){};
+
+        this.generate_double = function(){};
+
+        this.prune_tgraph = function(){};
+
+        this.search_tgraph = function(){};
+
+        this.prune_links = function(){};
+
+        this.combine_fragments  = function(){};
+
+        this.get_related_links = function(){};
+
+        this.worker_function = function(){};
+
+        this.pick_random = function(){};
+
+        this.calculate_candidate = function(){};
+
+        this.reverse_link = function(){};
+
+        this.get_chance = function(){};
+
+        
+
     }
-    
     return Cygen;
 }));
--- test/spec.js
+++ test/spec.js
@@ -11,8 +11,19 @@ lib = require('../lib/cygen.js');
 describe('Cygen.js Basic Tests', function() {
   it('should be able to say hello!', function() {
     var cygen = new lib.Cygen();
-    var answer = cygen.say_hello();
-    assert.equal(answer,'Hello');
+    var answer = cygen.show_version();
+    assert.equal(answer,'Version: 1.1.0');
   })
+});
 
-})
+describe('Cygen.js Intermediate Tests', function() {
+  it('should *', function() {
+    
+  })
+});
+
+describe('Cygen.js Advanced Tests', function() {
+  it('should *', function() {
+    
+  })
+});
